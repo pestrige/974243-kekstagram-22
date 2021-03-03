ae54c58abc12b0993e0a7fb534531cbdf2ab2a95
pestrige <pestrige@gmail.com>
2021-03-03 20:23:49 +0300

    Добавил сортировку

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 import { getData } from './data.js';
-import { thumbnails } from './create-thumbnails.js';
+import { renderThumbnails } from './render-thumbnails.js';
 import { showPostPreview } from './show-post.js';
 import { setPostForm } from './post-form.js';
 import {hideImgEditPopup} from './popup-img.js';
 import { showMessage } from './util.js';
+import { showFilterContainer, setFilterClick } from './posts-filters.js';
 import './scale-img.js';
 import './preview-filters.js';
 import './slider.js';
 
-getData(thumbnails, showPostPreview);
+getData((data) => {
+  renderThumbnails(data);
+  showPostPreview(data);
+  showFilterContainer();
+  setFilterClick(data, renderThumbnails);
+});
+
 setPostForm(
   () => {
     hideImgEditPopup();
