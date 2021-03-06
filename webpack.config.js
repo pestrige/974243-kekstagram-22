fb4c9da332f8e7690ac1d04e54649d9e9e210cd0
pestrige <pestrige@gmail.com>
2021-03-06 20:14:12 +0300

    Добавил сборку проекта

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './source/js/main.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'build/js'),
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: '../css/nouislider.css',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+};
